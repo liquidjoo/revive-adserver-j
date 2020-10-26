c56ffa755313ed354223e9ab76239b913608146f
liquidjoo <liquidjoo@gmail.com>
2020-10-26 22:59:02 +0900

    init : project init

---
+++ src/main/java/com/github/liquidjoo/reviveadserverj/ReviveAdserverJApplication.java
@@ -0,0 +1,13 @@
+package com.github.liquidjoo.reviveadserverj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviveAdserverJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviveAdserverJApplication.class, args);
+    }
+
+}
